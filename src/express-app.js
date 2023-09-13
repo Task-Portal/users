3bd43b1b7977a95461462687dfbc5bd77f9fb0c8
[Alex Tomson] <[lion1987.fm@gmail.com]>
2023-09-13 12:18:06 +0300

    url in build changed

--- src/express-app.js
+++ src/express-app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const { position, user} = require("./api");
-// const HandleErrors = require("./utils/error-handler");
 const cookieParser = require("cookie-parser");
 
 module.exports = async (app, unitOfWork) => {
@@ -9,20 +8,14 @@ module.exports = async (app, unitOfWork) => {
     app.use(cookieParser());
     app.use(express.json({ limit: "5mb" }));
     app.use(express.urlencoded({ extended: true, limit: "5mb" }));
-    app.use(cors());
-    app.use(express.static(__dirname + "/public"));
+    app.use(express.static("build"));
     app.disable("x-powered-by");
 
-
+    
     user(app, unitOfWork);
     position(app, unitOfWork);
     
  
-
-    app.use(async (req, res, next) => {
-      console.log("Req", req);
-      next();
-    });
 
     app.use(async(req,res,next)=>{
       res.status(404).json({
